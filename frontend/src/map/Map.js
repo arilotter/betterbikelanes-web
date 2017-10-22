2a9230c5c22081aa5bb9c01529ddce89d09f9c42
Ari Lotter <100621763@oncampus.local>
2017-10-22 09:23:46 -0400

    api key

--- frontend/src/map/Map.js
+++ frontend/src/map/Map.js
@@ -3,25 +3,29 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import Sensor from "./Sensor";
 
-
 export default class SimpleMap extends React.Component {
   static defaultProps = {
-    center:{lat:43.6665711, lng:-79.4020446},
+    center: { lat: 43.6665711, lng: -79.4020446 },
     zoom: 13
   };
 
   render() {
-    const sensorsOnMap = this.props.sensors.map(({ uuid, lat, lng, count, dir }) => (
-      <Sensor key={uuid} lat={lat} lng={lng} count={count} dir={dir} />
-    ));
-  
+    const sensorsOnMap = this.props.sensors.map(
+      ({ uuid, lat, lng, count, dir }) => (
+        <Sensor key={uuid} lat={lat} lng={lng} count={count} dir={dir} />
+      )
+    );
+
     return (
-       <GoogleMapReact
+      <GoogleMapReact
         defaultCenter={this.props.center}
         defaultZoom={this.props.zoom}
+        bootstrapURLKeys={{
+          key: "AIzaSyCl4kR-p0ieKib-mE4RD7TjQRY6w_MnmyI",
+          language: "en"
+        }}
       >
-      {sensorsOnMap}
-
+        {sensorsOnMap}
       </GoogleMapReact>
     );
   }
